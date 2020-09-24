f86c0e3ffb8445e5d75667774e2d5bd28348f90e
Márcio Vinícius <marcio.campos@outlook.com>
2020-09-24 14:36:41 -0300

    110. Hiding Scroll Bars

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -8,6 +8,7 @@ const ResultsList = ({ title, results }) => {
       <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal={true}
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtracto={(result) => result.id}
         renderItem={({ item }) => {
