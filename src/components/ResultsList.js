237710e6fbaae444a7a5cb4a1567920387917075
Márcio Vinícius <marcio.campos@outlook.com>
2020-09-24 14:32:40 -0300

    109. A Few More Styling Issues

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultsDetail from "./ResultsDetails";
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal={true}
@@ -22,6 +22,11 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
   },
 });
 
