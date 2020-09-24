073dca756bd25a51f126a6c3649f37ed93b0a1b9
Márcio Vinícius <marcio.campos@outlook.com>
2020-09-24 11:12:26 -0300

    103.Extracting Hook Logic

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "Belo Horizonte, Brasil",
+        },
+      });
+      console.log(response.data);
+      setResults(response.data.businesses);
+      setErrorMessage("");
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
